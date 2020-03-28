aa6d9f6fe61d890cd367b54ed6990418675b2c18
rainbow_bamboo <ada@tinyproblems.com>
2020-03-28 13:27:31 -0400

    attempting to parse url params

--- src/clj/coronavirus_charts/routes/home.clj
+++ src/clj/coronavirus_charts/routes/home.clj
@@ -4,18 +4,47 @@
    [clojure.java.io :as io]
    [coronavirus-charts.middleware :as middleware]
    [ring.util.response]
-   [ring.util.http-response :as response]))
+   [ring.util.http-response :as response]
+   [ring.middleware.params :as params]
+   [clara.rules :refer :all]))
 
 (defn home-page [request]
   (layout/render request "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
 
 (defn about-page [request]
+  (println (:query-params request))
   (layout/render request "about.html"))
 
 (defn home-routes []
   [""
    {:middleware [middleware/wrap-csrf
+                 params/wrap-params
                  middleware/wrap-formats]}
    ["/" {:get home-page}]
+   ["/t/*" {:get about-page}]
    ["/about" {:get about-page}]])
 
+
+(defrecord WebRequest [url])
+(defrecord ParsedRequest [url arguments])
+
+(defrecord ChartRequest [url type])
+(defrecord LocationRequest [url location])
+(defrecord TimeRequest [url time])
+
+(defrecord BarChart [url code valid-time])
+
+(defrule parse-request
+  "Whenever there's a WebRequest, create a corresponding ParsedRequest"
+  [WebRequest (= ?url url)]
+  =>
+  (insert! (->ParsedRequest ?url ["Hello" 9])))
+
+(defquery get-parsed-request
+  [:?url]
+  [?request <- ParsedRequest (= ?url url)])
+
+(-> (mk-session)
+    (insert (->WebRequest "hh.to/tt"))
+    (fire-rules)
+    (get-parsed-request "hh.to/tt"))
